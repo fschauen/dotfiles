49eb1af27a6fcfd010b24b5894dca9fb46c1e2d8
Fernando Schauenburg <dev@schauenburg.me>
2022-02-07 00:33:59 +0100

    vim: add macros to execute lua chunk/file

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -1,2 +1,10 @@
 vim.bo.tabstop = 2
 
+local nmap = require 'fs.keymap'.nmap
+
+-- execute the current line
+nmap { '<leader>x', [[<cmd>call luaeval(getline("."))<cr>]] }
+
+-- save and execute the current file
+nmap { '<leader><leader>x', '<cmd>silent write | luafile %<cr>' }
+
