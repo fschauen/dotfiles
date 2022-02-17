46044ab516fbab624812b5caa2db306135f94dbb
Fernando Schauenburg <dev@schauenburg.me>
2022-02-17 22:24:08 +0100

    vim: add useful lua global functions

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,17 @@
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+RELOAD = function(...)
+  return require('plenary.reload').reload_module(...)
+end
+
+R = function(name)
+  RELOAD(name)
+  return require(name)
+end
+
 require 'fs.disable_builtin'
 require 'fs.options'
 require 'fs.plugins'
