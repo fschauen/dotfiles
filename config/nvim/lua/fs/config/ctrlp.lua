0be2a1b45e7031de03591c4036fcc1ad45b7705d
Fernando Schauenburg <dev@schauenburg.me>
2022-07-01 20:30:50 +0200

    vim: remove configuration for unused plugin (ctrlp)

--- config/nvim/lua/fs/config/ctrlp.lua
+++
@@ -1,8 +0,0 @@
-local setup = function()
-  vim.g.ctrlp_match_window  = 'bottom,order:ttb'
-  vim.g.ctrlp_switch_buffer = 0   -- open files in new buffer
-  vim.g.ctrlp_show_hidden   = 1   -- show hidden files
-end
-
-return { setup = setup }
-
