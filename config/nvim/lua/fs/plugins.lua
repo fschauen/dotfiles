9a83027d392b3e4313d711af413ffe4a0921180b
Fernando Schauenburg <dev@schauenburg.me>
2022-10-07 08:52:29 +0200

    vim: minor tweak of how `packer` is loaded

--- config/nvim/lua/fs/plugins.lua
+++ config/nvim/lua/fs/plugins.lua
@@ -1,11 +1,11 @@
-local packer = function()
+local packer = (function()
   local path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if vim.fn.empty(vim.fn.glob(path)) > 0 then
     local url = 'https://github.com/wbthomason/packer.nvim'
     vim.fn.system({'git', 'clone', '--depth', '1', url, path})
   end
   return require('packer')
-end
+end)()
 
 vim.g.better_whitespace_filetypes_blacklist = {
   'diff',
@@ -20,7 +20,7 @@ vim.g.vim_json_syntax_conceal = 0   -- Disable quote concealling.
 vim.g.VM_leader = '\\'
 vim.g.VM_silent_exit = 1
 
-return packer().startup(function(use)
+packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'nvim-lua/plenary.nvim'
 
