d9cb38c8b2cc58047ee79ffb79bc6ad75dd90ec4
Fernando Schauenburg <dev@schauenburg.me>
2022-12-18 20:31:00 +0100

    vim: move all configuration into the `user` module

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,10 +1,2 @@
-vim.g.mapleader = ' '
-
-require 'fs.disable_builtin'
-require 'fs.globals'
-require 'fs.options'
-require 'fs.plugins'
-require 'fs.keymaps'
-require 'fs.autocmds'
-require 'fs.filetypes'
+require 'user'
 
---
+++ config/nvim/lua/user/init.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = ' '
+
+require 'user.disable_builtin'
+require 'user.globals'
+require 'user.options'
+require 'user.plugins'
+require 'user.keymaps'
+require 'user.autocmds'
+require 'user.filetypes'
+
