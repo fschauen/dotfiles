96115787282ece7843fc7a5c7a787db57e14569b
Fernando Schauenburg <dev@schauenburg.me>
2023-07-08 21:51:11 +0200

    vim: replace packer.nvim with lazy.nvim

--- config/nvim/lua/user/init.lua
+++ config/nvim/lua/user/init.lua
@@ -3,8 +3,22 @@ vim.g.mapleader = ' '
 require 'user.disable_builtin'
 require 'user.globals'
 require 'user.options'
-require 'user.plugins'
 require 'user.keymaps'
 require 'user.autocmds'
 require 'user.filetypes'
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup 'user.plugins'
 
