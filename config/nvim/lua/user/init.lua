b4056586a2ac4881f9ab9d1aec4b2813cfc1d3be
Fernando Schauenburg <dev@schauenburg.me>
2023-08-06 19:30:18 +0200

    vim: rename keymaps -> keymap

--- config/nvim/lua/user/init.lua
+++ config/nvim/lua/user/init.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = ' '
 require 'user.disable_builtin'
 require 'user.globals'
 require 'user.options'
-require 'user.keymaps'
+require 'user.keymap'
 require 'user.autocmds'
 require 'user.filetypes'
 
