d71ed2cbda7998024df275fade8cb29451c2575a
Fernando Schauenburg <dev@schauenburg.me>
2023-08-07 21:50:49 +0200

    vim/fugitive: add mapping for running a git command

--- config/nvim/lua/fschauen/plugins/fugitive.lua
+++ config/nvim/lua/fschauen/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
   'tpope/vim-fugitive',
 
   config = function()
+    vim.keymap.set('n', '<leader>gg', ':Git ')
     vim.keymap.set('n', '<leader>gs', '<cmd>tab Git<cr>')
     vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<cr>')
   end,
