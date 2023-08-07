83ef57d4e2f61d35746ba8415354a93eeb07ab3c
Fernando Schauenburg <dev@schauenburg.me>
2023-08-07 21:56:36 +0200

    vim/whitespace: more ergonomic mappings

--- config/nvim/lua/fschauen/plugins/vim-better-whitespace.lua
+++ config/nvim/lua/fschauen/plugins/vim-better-whitespace.lua
@@ -13,9 +13,9 @@ return {
 
   config = function()
     vim.keymap.set('n', '<leader>ww', '<cmd>ToggleWhitespace<cr>')
-    vim.keymap.set('n', '<leader>wn', '<cmd>NextTrailingWhitespace<cr>')
-    vim.keymap.set('n', '<leader>wp', '<cmd>PrevTrailingWhitespace<cr>')
-    vim.keymap.set('n', '<leader>W', '<cmd>StripWhitespace<cr>')
+    vim.keymap.set('n', '<leader>wj', '<cmd>NextTrailingWhitespace<cr>')
+    vim.keymap.set('n', '<leader>wk', '<cmd>PrevTrailingWhitespace<cr>')
+    vim.keymap.set('n', '<leader>wd', '<cmd>StripWhitespace<cr>')
   end,
 }
 
