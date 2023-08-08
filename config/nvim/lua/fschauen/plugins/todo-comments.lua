1371715363c4953f545e715f0a0e9c291a45b28c
Fernando Schauenburg <dev@schauenburg.me>
2023-08-08 17:19:50 +0200

    vim/todo-comments: reference Telescope's prefix for mapping

--- config/nvim/lua/fschauen/plugins/todo-comments.lua
+++ config/nvim/lua/fschauen/plugins/todo-comments.lua
@@ -1,19 +1,18 @@
+local prefix = require('fschauen.telescope').prefix or '<leader>f'
+
 return {
   'folke/todo-comments.nvim',
 
   dependencies = {
     'nvim-lua/plenary.nvim'
   },
-
   config = true,
-
   event = {
     'BufReadPost',
     'BufNewFile'
   },
-
   keys = {
-    { '<leader>ft', '<cmd>TodoTelescope<cr>' },
+    { prefix .. 't', '<cmd>TodoTelescope<cr>' },
   },
 }
 
