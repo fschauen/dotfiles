0554f4cb5e0fb7a1c037481bb45de370fcfe0ce0
Fernando Schauenburg <dev@schauenburg.me>
2023-08-11 03:38:19 +0200

    vim/markdown-preview: lazy load on filetype detection

--- config/nvim/lua/fschauen/plugins/markdown-preview.lua
+++ config/nvim/lua/fschauen/plugins/markdown-preview.lua
@@ -1,5 +1,10 @@
 return {
   'iamcco/markdown-preview.nvim',
   build = 'cd app && npm install',
+  init = function(_)
+    vim.g.mkdp_theme = 'dark'
+  end,
+  event = {
+    'FileType markdown',
+  },
 }
-
