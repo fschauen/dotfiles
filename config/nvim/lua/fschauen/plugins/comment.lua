ce74aaef60c874294a70dbda3ed3de46a1d6a5e1
Fernando Schauenburg <dev@schauenburg.me>
2023-08-11 03:17:32 +0200

    vim: go back to vim-commentary, lazy loaded

--- config/nvim/lua/fschauen/plugins/comment.lua
+++ config/nvim/lua/fschauen/plugins/comment.lua
@@ -1,3 +1,9 @@
 return {
-  'numToStr/Comment.nvim',
+  'tpope/vim-commentary',
+  cmd = 'Commentary',
+  keys = {
+    { 'gc',  '<Plug>Commentary',    mode = { 'n', 'x', 'o' }, desc = 'Comment in/out' },
+    { 'gcc', '<Plug>CommentaryLine',             desc = 'Comment in/out line' },
+    { 'gcu', '<Plug>Commentary<Plug>Commentary', desc = 'Undo comment in/out' },
+  },
 }
