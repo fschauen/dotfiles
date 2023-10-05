3866ffecc1cb155fb34228631f4f28578e295869
Fernando Schauenburg <dev@schauenburg.me>
2023-10-05 22:35:05 +0200

    vim: move autocmds to separate file

--- config/nvim/lua/fschauen/init.lua
+++ config/nvim/lua/fschauen/init.lua
@@ -204,29 +204,7 @@ M.setup = function()
 
   require('fschauen.keymap').setup()
   require('fschauen.diagnostic').setup()
-
-  local group = vim.api.nvim_create_augroup('fschauen', { clear = true } )
-
-  vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Briefly highlight yanked text.',
-    group = group,
-    pattern = '*',
-    callback = function(_) vim.highlight.on_yank() end
-  })
-
-  vim.api.nvim_create_autocmd('InsertEnter', {
-    desc = 'Hide cursor line when entering insert mode.',
-    group = group,
-    pattern = '*',
-    callback = function(_) vim.opt.cursorlineopt = 'number' end
-  })
-
-  vim.api.nvim_create_autocmd('InsertLeave', {
-    desc = 'Show cursor line when leaving insert mode.',
-    group = group,
-    pattern = '*',
-    callback = function(_) vim.opt.cursorlineopt = 'both' end
-  })
+  require('fschauen.autocmd').setup()
 
   vim.filetype.add {
     pattern = {
