cb201df3d2808a03b606221759ca502139facaa3
Fernando Schauenburg <dev@schauenburg.me>
2023-12-13 15:32:14 +0100

    zsh: save history immediately

--- config/zsh/history.zsh
+++ config/zsh/history.zsh
@@ -1,3 +1,7 @@
+setopt APPEND_HISTORY     # Append history rather than overwrite.
+setopt EXTENDED_HISTORY   # Save beginning timestamp and duration.
+setopt INC_APPEND_HISTORY # Don't wait until shell exits to save history.
+
 HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/history"
 HISTSIZE=1000000
 SAVEHIST=1000000
