b08a8738113ba64c7032c2333c2cce83b4170169
Fernando Schauenburg <dev@schauenburg.me>
2024-02-08 20:25:11 +0100

    vim: update indent-blankline and fix breaking changes

--- config/nvim/lua/fschauen/plugins/indent-blankline.lua
+++ config/nvim/lua/fschauen/plugins/indent-blankline.lua
@@ -1,25 +1,32 @@
 local M = { 'lukas-reineke/indent-blankline.nvim' }
 
-M.keys = {
-  { '<leader>si', '<cmd>IndentBlanklineToggle<cr>' },
-}
-
-M.lazy = false -- trows an error when lazy loading
-
-local chars = { '│', '¦', '┆', '┊', '┊', '┊', '┊', '┊', '┊', '┊' }
-local show_first_level = false
-
 M.config = function()
-  require('indent_blankline').setup {
+  require('ibl').setup {
     enabled = false,
-    use_treesitter = true,
-    show_first_indent_level = show_first_level,
-    show_current_context = true,
-    show_trailing_blankline_indent = false,
-    char_list = chars,
-    context_char_list = chars,
-    indent_level = #chars + (not show_first_level and 1 or 0),
+    indent = {
+      char = '▏',
+    },
+    scope = {
+      char = '▎',
+      enabled = false,
+      show_start = false,
+      show_end = false,
+    },
   }
 end
+
+M.cmd = {
+  'IBLEnable',
+  'IBLDisable',
+  'IBLToggle',
+  'IBLEnableScope',
+  'IBLDisableScope',
+  'IBLToggleScope',
+}
+
+M.keys = {
+  { '<leader>si', '<cmd>IBLToggle<cr>' },
+  { '<leader>so', '<cmd>IBLToggleScope<cr>' },
+}
 
 return M
