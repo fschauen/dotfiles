a15e582567394a2dc2563877330295df6acda919
Fernando Schauenburg <dev@schauenburg.me>
2024-02-19 01:58:57 +0100

    vim: add plugin 'mhartington/formatter.nvim'

---
+++ config/nvim/lua/fschauen/plugins/formatter.lua
@@ -0,0 +1,32 @@
+local M = { 'mhartington/formatter.nvim' }
+
+M.cmd = {
+  'Format',
+  'FormatLock',
+  'FormatWrite',
+  'FormatWriteLock',
+}
+
+M.keys = {
+  { '<leader>F', '<cmd>Format<cr>', desc = '󰉼 Format file' },
+}
+
+M.opts = function( --[[plugin]] _, opts)
+  local ft = require('formatter.filetypes')
+  return vim.tbl_deep_extend('force', opts or {}, {
+    filetype = {
+      c        = { ft.c.clangformat },
+      cmake    = { ft.cmake.cmakeformat },
+      cpp      = { ft.cpp.clangformat },
+      cs       = { ft.cs.clangformat },
+      json     = { ft.cs.prettier },
+      lua      = { ft.lua.stylua },
+      markdown = { ft.markdown.prettier },
+      python   = {}, -- TODO: pick one
+      sh       = { ft.sh.shfmt },
+      zsh      = { ft.zsh.beautysh },
+    }
+  })
+end
+
+return M
